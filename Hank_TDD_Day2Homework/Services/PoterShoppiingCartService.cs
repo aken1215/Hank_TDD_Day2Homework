5aab7fdbc578b24b2b6958500811cf614b35926d
Hank <aken1215@hotmail.com>
2016-07-13 14:41:13 +0800

    實作 第一集買了一本，其他都沒買，價格應為100*1=100元

--- Hank_TDD_Day2Homework/Services/PoterShoppiingCartService.cs
+++ Hank_TDD_Day2Homework/Services/PoterShoppiingCartService.cs
@@ -9,14 +9,23 @@ namespace Hank_TDD_Day2Homework.Services
 {
     public class PoterShoppiingCartService : IShoppingCartService<PoterBook>
     {
+        private int _total { get; set; }
+        private ICollection<PoterBook> _books { get; set; }
+
+        public PoterShoppiingCartService()
+        {
+            this._books = new List<PoterBook>();
+        }
+
         public int Bill()
         {
-            throw new NotImplementedException();
+            _total=_books.Select(i => i.Pirce).Sum();
+            return _total;
         }
 
         public void OrderProduct(PoterBook product)
         {
-
+            _books.Add(product);
         }
     }
 }
